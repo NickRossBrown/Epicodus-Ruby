e95c18b4b854c568def7b5963d9ab395836a1f59
Craig Wann and nick brown <nick@>
2018-06-26 14:29:39 -0700

    add double digit numbers

--- spec/numbers_spec.rb
+++ spec/numbers_spec.rb
@@ -3,11 +3,11 @@ require('pry')
 require('numbers')
 
 describe ('Number#numbers') do
-  it("returns a ONE for a number 1") do
+  it("returns a word for a single digit number") do
     num = Number.new(9)
     expect(num.number_to_word()).to(eq("nine"))
   end
-  it("returns a ONE for a number 1") do
+  it("returns a word for double digits up to 19") do
     num = Number.new(17)
     expect(num.number_to_word()).to(eq("seventeen"))
   end
