e7f0e2ad639fa4814dc23aec8d9327675951ad6b
Nick Brown and Reese Glasscock <nick+reese@>
2018-06-27 10:04:45 -0700

    initialize player 1 in wins method

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,11 +1,14 @@
 class RPS
-  # def initialize (player1)
-  #   @player1 = player1
-  # end
+  def initialize (player1)
+    @player1 = player1
+  end
 
-  def wins? (player1, player2)
-    if player1 == 'rock' && player2 == 'paper'
+  def wins? (player2)
+    if @player1 == 'rock' && player2 == 'paper'
       false
+    elsif @player1 == 'rock' && player2 == 'scissors'
+      true
+
     end
   end
 
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -3,8 +3,12 @@ require 'pry'
 require 'rps'
 
 describe ('play rock paper scissors') do
-  it("returns true if rock is the object and paper is the argument") do
-    game = RPS.new()
-    expect(game.wins?("rock", "paper")).to(eq(false))
+  it("returns false if rock is the object and paper is the argument") do
+    game = RPS.new('rock')
+    expect(game.wins?("paper")).to(eq(false))
+  end
+  it("returns true if rock is the object and scissors is the argument") do
+    game = RPS.new('rock')
+    expect(game.wins?("scissors")).to(eq(true))
   end
 end
