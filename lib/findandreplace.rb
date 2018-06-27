11ffa1f6fee15cba4fc3d0ea9203d77b39d410e0
Nick Brown and Reese Glasscock <nick+reese@>
2018-06-27 11:26:54 -0700

    first test pass

---
+++ lib/findandreplace.rb
@@ -0,0 +1,18 @@
+class Words
+  def initialize (phrase)
+  @phrase = phrase
+ end
+ def replace ()
+  sentence_words = {'world' => 'universe'}
+  split_phrase = @phrase.split(" ")
+  returned_phrase = []
+  split_phrase.each do |index|
+    if (sentence_words.to_s()).include?(index) == true
+      returned_phrase.push(sentence_words.fetch(index))
+    else
+      returned_phrase.push(index)
+    end
+  end
+  returned_phrase.join(" ")
+ end
+end
--- spec/findandreplace_spec.rb
+++ spec/findandreplace_spec.rb
@@ -4,8 +4,8 @@ require 'pry'
 
 describe ('find and replace') do
   it "finds and replaces words in a string" do
-    sentence = Words.new()
-    expect(sentence.replace("Hello world")).to(eq("Hello universe"))
+    sentence = Words.new("Hello world")
+    expect(sentence.replace()).to(eq("Hello universe"))
 
   end
 end
