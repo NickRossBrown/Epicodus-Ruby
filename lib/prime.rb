498b2c1d1d8be594caf83e6c29fed70c1e3ab0b8
Craig Wann and Nick Brown <nick@>
2018-06-28 14:03:25 -0700

    test 4 pass, returns all primes from 2-100

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,10 +1,17 @@
 class Prime
   def initialize (integer)
     @integer = integer
+    @array = (2..@integer).to_a
   end
 
-  def create_array()
-    array = (1..@integer).to_a
+  def return_array_primes()
+    prime_array = []
+    @array.each do |number|
 
+      # prime_array.push(number)
+      @array = @array.reject {|r| r > number && r % number == 0}
+    end
+    puts @array
+    @array
   end
 end
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -3,8 +3,20 @@ require('pry')
 require('prime')
 
 describe ('Prime#prime') do
-  it("creates an array from 1 to input") do
+  it("creates an array from 2 to input") do
     change = Prime.new(4)
-    expect(change.create_array()).to(eq([1,2,3,4]))
+    expect(change.return_array_primes()).to(eq([2,3]))
+  end
+  it("creates an array from 2 to input rejecting all the numbers divisible by 2") do
+    change = Prime.new(14)
+    expect(change.return_array_primes()).to(eq([2,3,5,7,11,13]))
+  end
+  it("creates an array from 2 to input rejecting all the numbers divisible by 2") do
+    change = Prime.new(35)
+    expect(change.return_array_primes()).to(eq([2,3,5,7,11,13,17,19,23,29,31]))
+  end
+  it("creates an array from 2 to input rejecting all the numbers divisible by 2") do
+    change = Prime.new(100)
+    expect(change.return_array_primes()).to(eq([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]))
   end
 end
