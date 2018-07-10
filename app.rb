3d438a52b81caaa14a9b421caccc6dcb3f0f6c00
Craig Wann and Eric Shull <craig@>
2018-07-10 10:01:53 -0700

    12 examples, 0 failures

--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
-require("sinatra")
-require("sinatra/reloader")
-also_reload("lib/**/*.rb")
-require("./lib/task")
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/task')
+require('./lib/list')
+also_reload('lib/**/*.rb')
 require("pg")
 
 DB = PG.connect({:dbname => "to_do"})
-a
+
+
+get("/") do
+  erb(:index)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:success)
+ end
+
+ get('/lists') do
+   @lists = List.all()
+   erb(:lists)
+ end
